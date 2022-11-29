ac97666202572ea42a10aac955452e6feaa6f42e
skyofdwarf <skyofdwarf@gmail.com>
2022-11-29 18:56:53 +0900

    Add SnapKit package

--- Projects/RadioBox/Project.swift
+++ Projects/RadioBox/Project.swift
@@ -31,6 +31,7 @@ let bundleIdPrefix = "com.dwarfini"
 let dependencies: [TargetDependency] = [
     .project(target: "RadioBrowser", path: .relativeToRoot("Projects/RadioBrowser")),
     .package(product: "Stevia"),
+    .package(product: "SnapKit"),
     .package(product: "Kingfisher"),
     .package(product: "Then"),
     .package(product: "RDXVM"),
@@ -44,6 +45,7 @@ let project = Project(name: name,
                       organizationName: organization,
                       packages: [
                         .package(url: "https://github.com/freshOS/Stevia", from: "5.1.0"),
+                        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.6.0"),
                         .package(url: "https://github.com/onevcat/Kingfisher", from: "7.0.0"),
                         .package(url: "https://github.com/devxoul/Then", from: "2.0.0"),
                         .package(url: "https://github.com/skyofdwarf/RDXVM", .branch("develop"))
